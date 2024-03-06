6b4fd4c511f5a0ac475ec6dbd425bf69ff796d05
Emerson Ribeiro <emersonrjr03@gmail.com>
2024-03-06 12:54:35 +0000

    changing provider return

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,9 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "v2")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
         ]);
     }
 
